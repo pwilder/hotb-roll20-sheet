375bbff92af1f2af7b8d6d32ed45f9b21912e6cd
Philip Wilder <philip.wilder@gmail.com>
2024-12-17 20:14:08 -0500

    Have working risk roll

--- source/sheetworkers.js
+++ source/sheetworkers.js
@@ -1,26 +1,79 @@
 // Automatically updates the number of d6s based on the selected attribute
-on("change:roll_virtue change:Strength change:Cunning change:Courage change:Beauty change:Wisdom change:Prowess", function() {
-    console.log("Sheet Worker triggered");
-    getAttrs(["roll_virtue", "Strength", "Cunning", "Courage", "Beauty", "Wisdom", "Prowess"], function(values) {
-        let selectedAttribute = values.roll_virtue;
-        let numberOfDice = 0;
 
-        switch (selectedAttribute) {
-            case "Strength":
-                numberOfDice = parseInt(values.Strength) || 0;
-                break;
-            // case "dexterity":
-            default:
-                numberOfDice = parseInt(values.Cunning) + parseInt(values.Strength) || 0;
-                break;
-            // case "constitution":
-            //     numberOfDice = parseInt(values.constitution) || 0;
-            //     break;
+const selection = ["roll_virtue"];
+const virtues = ["Strength", "Cunning", "Courage", "Beauty", "Wisdome", "Prowess"];
+const bonusDice = ["public-name", "invoke", "tag", "bonus-dice"];
+const negativeDice = ["wager"];
+
+const attrs = [...selection, ...virtues, ...bonusDice, ...negativeDice];
+const events = attrs.map((val) => `change:${val}`);
+
+const ON = 'on';
+
+const safeParseInt = (val) => {
+    try {
+        return parseInt(val)
+    } catch (err) {
+        return 0;
+    }
+}
+
+on(events.join(" "), function() {
+    console.log("Sheet Worker triggered");
+    getAttrs(attrs, function(values) {
+        console.log(JSON.stringify(values));
+        const selectedAttribute = values.roll_virtue;
+        const wager = safeParseInt(values.wager);
+
+
+        if (wager < 0) {
+            setAttrs({
+                risk_template: `Positive values only for wagers please.`,
+            });
+            return;
         }
+
+        const bonusDice = safeParseInt(values['bonus-dice']);
+
+        const templateObj = {
+            name: 'Risk Roll',
+            [selectedAttribute]: parseInt(values[selectedAttribute]),
+            ...(values['public-name'] === ON && { 'Public Name': 1 }),
+            ...(values.invoke === ON && { 'Invoke': 3 }),
+            ...(values.tag === ON && { 'Tag': 2 }),
+            ...(wager && { 'Wager': wager }),
+            ...(bonusDice && { 'Bonus Dice': bonusDice }),
+        };
+
+        const finalDieCount = Object.entries(templateObj).reduce((acc, [key, value]) => {
+            if (!Number.isSafeInteger(value)) {
+                return acc;
+            }
+            if (key === 'Wager') {
+                acc -= value;
+            } else {
+                acc += value;
+            }
+            return acc;
+        }, 0);
+
+        if (finalDieCount < 0) {
+            setAttrs({
+                risk_template: `You are rolling [${finalDieCount}] dice, maybe lower you wagers?`,
+            });
+            return;
+        }
+
+        templateObj.Result = `[[${finalDieCount}d6]]`;
+
+        let templateString = '&{template:default}';
+        templateString += Object.entries(templateObj).map(([key, value]) => `{{${key}=${value}}}`).join(' ');
+
+
 
         // Set the number of d6s to roll
         setAttrs({
-            num_of_d6: numberOfDice,
+            risk_template: templateString,
         });
     });
 });
